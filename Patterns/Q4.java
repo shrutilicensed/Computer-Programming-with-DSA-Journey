aa4f3b6a2270451a3a82a2cc4c6ed23aed214627
shruti <shruti.licensed@gmail.com>
2023-03-19 20:16:44 +0530

    corrected Q4

--- Patterns/Q4.java
+++ Patterns/Q4.java
@@ -4,19 +4,14 @@ public class Q4 {
   public static void main(String[] args){
     Scanner scan= new Scanner(System.in);
     int num= scan.nextInt();
-    int num0= num-1;
     scan.close();
-    for( int i=0; i<=num; i++){
-      for (int j=0; j<num; j++){
-        if(j < num0){
-          System.out.print(" ");
-        }
-        else{
-          System.out.print("*");
-        }
-        num0= num0-1;
+    for( int i=1; i<=num; i++){
+      for(int space=num-i; space>0; space--){
+        System.out.print(" ");
       }
-      num0= num-1;
+      for(int star= 1; star<=i; star++){
+        System.out.print("*");
+      }
       System.out.println();
     }
   }
