4884d4d1a6b24929d7ef2de253c9989778bb9f5c
shruti <shruti.licensed@gmail.com>
2023-03-26 22:18:20 +0530

    half solved Q17 pattern

---
+++ Patterns/Q17.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Q17 {
+  public static void main(String[] args) {
+    Scanner scan= new Scanner(System.in);
+    int num= scan.nextInt();
+    for(int row=1; row<=num/2; row++){
+      // for(int star=1; star<; )
+      System.out.println();
+    }
+  }
+}
