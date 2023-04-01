e00b33510ecdadfed5a9e80be8ac667100d2de89
shruti <shruti.licensed@gmail.com>
2023-04-01 19:47:27 +0530

    solved 2 ques on function

---
+++ LearningFunctions/BaseToNumber.java
@@ -0,0 +1,11 @@
+package LearningFunctions;
+import java.util.Scanner;
+public class BaseToNumber {
+  public void calcBaseToNum(int n, int b){
+
+  }
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    
+  }
+}
