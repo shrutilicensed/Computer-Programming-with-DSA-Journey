ee1f3bb8f9c85331414de2e39430445a43625698
shruti <shruti.licensed@gmail.com>
2023-04-24 22:11:40 +0530

    added linkedlist package

---
+++ LinkedLists/reversalOfLL.java
@@ -0,0 +1,18 @@
+package LinkedLists;
+import java.util.*;
+
+public class reversalOfLL {
+public static void main(String[] args) {
+  Scanner scan = new Scanner(System.in);
+  
+}
+}
+
+class Node{
+  int data;
+  Node next;
+
+  Node(int value){
+    this.data = value;
+  }
+}
